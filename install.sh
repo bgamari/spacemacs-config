039f260ec2c16594894b7a256af3d6a09d576bd5
Ben Gamari <bgamari.foss@gmail.com>
2015-05-02 12:47:17 -0400

    install: Install spacemacs and haskell utilities

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ ! -d $HOME/.emacs.d ]; then
+    git clone git://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+fi
+
+ln -s .emacs.d/init.el $HOME/.emacs
 ln -s .spacemacs-config/spacemacs $HOME/.spacemacs
 
+cabal install ghc-mod stylish-haskell hlint
