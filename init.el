9e607cf71f61b669971355feb4b74f97e9c67136
Ben Gamari <ben@smart-cactus.org>
2016-10-29 13:25:09 -0400

    Add nixos-mode

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
      html javascript markdown git github emacs-lisp shell
      idris asciidoc rust purescript
      tracwiki-mode
+     nixos-mode
      (haskell :variables haskell-enable-ghc-mod-support nil)
      )
    dotspacemacs-excluded-packages '()
