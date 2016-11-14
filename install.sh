69aa7ba5b71812f91f882ea97d66cd1ed76267d4
Ben Gamari <ben@smart-cactus.org>
2016-11-14 08:04:56 -0500

    Fix spacemacs submodule

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 git submodule update --init
 if [ ! -h $HOME/.emacs.d ]; then
-    ln -s $HOME/.spacemacs.d/spacemacs $HOME/.emacs.d
+    ln -s $HOME/.spacemacs.d/spacemacs-root $HOME/.emacs.d
 fi
 
 cabal install ghc-mod stylish-haskell hlint
